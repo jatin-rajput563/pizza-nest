5f7bcaf56ab6692381ced8b712b120a294a80995
jatin-rajput563 <jkt3298@gmail.com>
2025-07-16 12:35:28 +0530

    largepizza-section-complete

--- src/components/LargePizza.jsx
+++ src/components/LargePizza.jsx
@@ -1,36 +1,41 @@
 import React from "react";
-// import CustomDescription from "./common/CustomDescription";
-// import CustomHeading from "./common/CustomHeading";
-// import CustomBtn from "./common/CustomBtn";
-// import LargePizzaImg from "../assets/images/png/largeTwo-pizza-img.png";
+import CustomHeading from "./common/CustomHeading";
+import CustomBtn from "./common/CustomBtn";
+import LargePizzaImg from "../assets/images/png/tasty-pizza-img.png";
 
 const LargePizza = () => {
   return (
     <>
-      {/* <div className="bg-[url(../src/assets/images/png/largePizza-bg.png)] bg-cover bg-center bg-no-repeat pt-[78px] pb-[94px]">
-        <div className="max-w-[1140px] mx-auto px-4">
-          <div className="flex flex-wrap flex-row -mx-3 ">
-            <div className="w-1/2">
-              <CustomDescription
-                DescClass="font-semibold bg-[linear-gradient(85.95deg,_#EC6112_1.54%,_#FF902E_98.46%)] bg-clip-text text-transparent"
-                descText="Buy 1 Pizza, Get 1 Free!"
-              />
+      <div className="bg-[url(../src/assets/images/png/tasty-bg-img.png)] bg-cover px-3">
+        <div className="max-w-[1194px] mx-auto">
+          <div className="pt-[17px] pb-[33px] flex max-sm:flex-wrap justify-center gap-5 lg:gap-[44px] items-center">
+            <div className="md:max-w-[400px] lg:max-w-[597px]">
+              <p className="bg-[linear-gradient(85.95deg,_#EC6112_1.54%,_#FF902E_98.46%)] bg-clip-text text-transparent pb-0.5 font-semibold text-2xl md:text-[28px] leading-[140%] ">
+                Buy 1 Pizza, Get 1 Free!
+              </p>
               <CustomHeading
-                headClass="text-white pt-[2px]"
+                headClass="text-white !text-[30px] md:!text-[40px] lg:!text-[56px]"
                 headText="Medium & Large pizzas"
               />
-              <CustomDescription
-                DescClass="font-semibold bg-[linear-gradient(85.95deg,_#EC6112_1.54%,_#FF902E_98.46%)] bg-clip-text text-transparent pt-[4px]"
-                descText="Limited Offer"
+
+              <p className="bg-[linear-gradient(85.95deg,_#EC6112_1.54%,_#FF902E_98.46%)] bg-clip-text text-transparent font-semibold text-2xl md:text-[28px] leading-[140%] pt-1 w-full">
+                Limited Offer
+              </p>
+              <CustomBtn
+                btnClass="!px-[32.7px] mt-3"
+                btnText="Order Now"
               />
-              <CustomBtn btnClass="!px-[32.7px] mt-4" btnText="Order Now" />
             </div>
-            <div className="w-1/2">
-              <img className="max-w-[553px]" src={LargePizzaImg} alt="LargePizzaImg" />
+            <div className="relative flex items-center">
+              <img
+                className="max-w-[400px] pointer-events-none lg:max-w-[553px] w-full"
+                src={LargePizzaImg}
+                alt="pizza-img"
+              />
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 };
